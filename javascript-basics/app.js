3fec17a7aa8bcad07d3543f929488559214d9b28
Dominik Piegdoń <domi.pieg@gmail.com>
2022-03-14 21:11:21 +0100

    JS basic + obfuscated homework

---
+++ javascript-basics/app.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', () => {
+    console.log('My alert');
+
+    array
+})
+
+
+const firstName = "Dominik";
+const firstname = "Dominik";
+
+
+const array = [5];
+
+array.push(1);
+array.push(7);
+
+console.log(array);
