60134b50381b967d77c4548428e8a037a46d1974
Dominik Piegdoń <domi.pieg@gmail.com>
2022-03-14 21:57:54 +0100

    JS basic + obfuscated homework

--- javascript-basics/app.js
+++ javascript-basics/app.js
@@ -17,3 +17,48 @@ array.push(1);
 array.push(7);
 
 console.log(array);
+
+const concat = "a" + (2 + 3) + "b";
+
+const concatSecondApproach = `a${2 + 3}b`;
+
+
+let integer = 0;
+//0
+
+integer += 10;
+//10
+
+integer++;
+//11
+
+integer--;
+//10
+
+integer *= 5;
+//50
+
+integer /= 5;
+//10
+
+integer = integer % 2; 
+//0 - rest of division 10 by 2
+
+const arr = [];
+
+function helloBob() {
+    console.log("Hello there bob");
+}
+
+helloBob();
+
+
+
+function countLari(a, b, c, d) {
+  return a + b + c + d;
+}
+
+// Keti asks mom to count lari
+const howMuchIHave = countLari(2, 2, 1, 5);
+
+alert("Grandma, I have: " + howMuchIHave + " lari");
