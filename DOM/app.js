e4e8cdb7ba2a7bcf4ea88c49a5750b620b52ca82
Dominik Piegdoń <domi.pieg@gmail.com>
2022-03-30 22:23:11 +0200

    dom

---
+++ DOM/app.js
@@ -0,0 +1,41 @@
+//window.alert("");
+
+window.document.getElementById("btn");
+
+document.getElementById("title");
+
+
+// const allLiElements = [
+//     document.children[0].children[1].children[2].children[0],
+//     document.children[0].children[1].children[2].children[1],
+//     document.children[0].children[1].children[2].children[2],
+//     document.children[0].children[1].children[2].children[3]
+// ];
+
+// // const collectionOfLiElements = document.getElementsByTagName("li");
+// const collectionOfLiElements = document.getElementsByClassName("person");
+// console.log(collectionOfLiElements);
+
+// const arrayOfLiElements = Array.from(collectionOfLiElements);
+// console.log(arrayOfLiElements);
+
+// //const allLiElements = [...document.getElementsByTagName("li")];
+
+// arrayOfLiElements.forEach(each => {
+//     each.innerHTML = each.innerHTML + " is happy";
+// })
+
+
+// const car = document.querySelector("#audi");
+// console.log(car);
+
+// const carCollection = document.querySelectorAll("#audi");
+// console.log(carCollection[0]);
+
+
+const carsList = document.querySelector("#cars");
+console.log(carsList);
+
+// carsList.lastChild
+// carsList.firstChild
+
